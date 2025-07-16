5e800d68a5d81ee9c665622d974de0242c6ba928
JKyEun <j56237@naver.com>
2025-07-16 21:30:57 +0900

    feat: input file로 camera 접근 방법 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-vite-plugin';
 
 export default defineConfig({
   plugins: [react(), tanstackRouter()],
+  server: {
+    allowedHosts: ['0cfb2f639498.ngrok-free.app']
+  }
 });
