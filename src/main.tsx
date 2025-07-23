a178d26939ed2002034334118855bc3118cd6e69
minsgy <alstjrchl97@gmail.com>
2025-07-23 21:57:19 +0900

    feat: Hotjar 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import Hotjar from "@hotjar/browser";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
@@ -15,3 +16,10 @@ ReactDOM.createRoot(rootElement).render(
     </Theme>
   </React.StrictMode>
 );
+
+const siteId = 6472888;
+const hotjarVersion = 6;
+
+if (import.meta.env.PROD) {
+  Hotjar.init(siteId, hotjarVersion);
+}
