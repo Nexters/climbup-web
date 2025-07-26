ceb82814d552dc9bcad5641702c995a38ef2b1e9
JKyEun <j56237@naver.com>
2025-07-26 19:41:40 +0900

    feat: mission header 설정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,32 +1,13 @@
-import { Container, Flex } from "@radix-ui/themes";
-import {
-  createRootRoute,
-  Outlet,
-  useRouterState,
-} from "@tanstack/react-router";
-import Header from "./mission/-components/MissionHeader";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
-const ROUTES_WITHOUT_HEADER = ["/", "/difficulty", "/gym"];
-
 function RootComponent() {
-  const routerState = useRouterState();
-  const hasHeader = !ROUTES_WITHOUT_HEADER.includes(
-    routerState.location.pathname
-  );
-
   return (
-    <Flex
-      direction="column"
-      className="bg-neutral-200 min-h-screen max-w-[600px] mx-auto outline outline-1 outline-gray-200 -outline-offset-1"
-    >
-      {hasHeader && <Header />}
-      <Container py="4" px="4">
-        <Outlet />
-      </Container>
-    </Flex>
+    <div className="bg-neutral-200 min-h-screen max-w-[600px] mx-auto outline outline-1 outline-gray-200 -outline-offset-1">
+      <Outlet />
+    </div>
   );
 }
