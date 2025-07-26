933c8c9623c13469bd4a6e9d55873374f60430ec
minsgy <alstjrchl97@gmail.com>
2025-07-26 22:49:45 +0900

    feat: Tailwind CSS 구성 요소 추가 (색상, 스크롤 숨기기, 타이포그래피)

---
+++ tailwind-config/typography.ts
@@ -0,0 +1,108 @@
+// Typography 컴포넌트 토큰 정의
+const pretendard =
+  '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif';
+const montserrat = '"Montserrat, sans-serif';
+const typography = {
+  // Pretendard
+  ".t-p-42-b": {
+    fontFamily: pretendard,
+    fontSize: "42px",
+    fontWeight: "700",
+    lineHeight: "1.4",
+    letterSpacing: "-2.4%",
+  },
+  ".t-p-32-b": {
+    fontFamily: pretendard,
+    fontSize: "32px",
+    fontWeight: "700",
+    lineHeight: "1.4",
+    letterSpacing: "-2.4%",
+  },
+  ".t-p-28-b": {
+    fontFamily: pretendard,
+    fontSize: "28px",
+    fontWeight: "700",
+    lineHeight: "1.4",
+    letterSpacing: "-2.4%",
+  },
+  ".t-p-22-sb": {
+    fontFamily: pretendard,
+    fontSize: "22px",
+    fontWeight: "600",
+    lineHeight: "1.4",
+    letterSpacing: "-2.4%",
+  },
+  ".t-p-20-sb": {
+    fontFamily: pretendard,
+    fontSize: "20px",
+    fontWeight: "600",
+    lineHeight: "1.4",
+    letterSpacing: "-2.4%",
+  },
+  ".t-p-18-sb": {
+    fontFamily: pretendard,
+    fontSize: "18px",
+    fontWeight: "600",
+    lineHeight: "1.4",
+    letterSpacing: "-2.2%",
+  },
+  ".t-p-16-m": {
+    fontFamily: pretendard,
+    fontSize: "16px",
+    fontWeight: "500",
+    lineHeight: "1.4",
+    letterSpacing: "-2.2%",
+  },
+  ".t-p-14-sb": {
+    fontFamily: pretendard,
+    fontSize: "14px",
+    fontWeight: "600",
+    lineHeight: "1.4",
+    letterSpacing: "-2.2%",
+  },
+  ".t-p-14-m": {
+    fontFamily: pretendard,
+    fontSize: "14px",
+    fontWeight: "500",
+    lineHeight: "1.4",
+    letterSpacing: "-2.2%",
+  },
+  ".t-p-12-m": {
+    fontFamily: pretendard,
+    fontSize: "12px",
+    fontWeight: "500",
+    lineHeight: "1.4",
+    letterSpacing: "-2.2%",
+  },
+  ".t-p-10-sb": {
+    fontFamily: pretendard,
+    fontSize: "10px",
+    fontWeight: "600",
+    lineHeight: "1.4",
+    letterSpacing: "-2.2%",
+  },
+  // Montserrat
+  ".t-m-56-b": {
+    fontFamily: montserrat,
+    fontSize: "56px",
+    fontWeight: "800",
+    lineHeight: "1.0",
+    letterSpacing: "-2.4%",
+  },
+  ".t-m-48-b": {
+    fontFamily: montserrat,
+    fontSize: "48px",
+    fontWeight: "800",
+    lineHeight: "1.0",
+    letterSpacing: "-2.4%",
+  },
+  ".t-m-24-b": {
+    fontFamily: montserrat,
+    fontSize: "24px",
+    fontWeight: "800",
+    lineHeight: "1.0",
+    letterSpacing: "-2.4%",
+  },
+};
+
+export default typography;
