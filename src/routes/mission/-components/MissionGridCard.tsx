fdcede0d74e43363dc90f14f57d302b9d7618393
JKyEun <j56237@naver.com>
2025-07-28 00:29:10 +0900

    feat: 미션 그리드 카드 스타일 구현

---
+++ src/routes/mission/-components/MissionGridCard.tsx
@@ -0,0 +1,57 @@
+import { Link } from "@tanstack/react-router";
+import { clsx } from "clsx";
+import Button from "@/components/Button";
+import LockIcon from "../../../components/icons/LockIcon";
+
+interface MissionGridCardProps {
+  missionId: string;
+  sectorName: string;
+  difficulty: string;
+  status?: "success" | "failed" | "not_tried";
+  isLocked?: boolean;
+}
+
+export default function MissionGridCard({
+  missionId,
+  sectorName,
+  difficulty,
+  status = "not_tried",
+  isLocked = false,
+}: MissionGridCardProps) {
+  return (
+    <Link
+      to="/mission/$missionId"
+      params={{ missionId }}
+      className="flex-[0_0_300px] h-[400px] rounded-[40px] overflow-hidden  border-8 border-neutral-100"
+    >
+      <div className="relative h-full p-5">
+        {status === "not_tried" && (
+          <img
+            src={`https://placehold.co/400x600/4D5761/FCFCFD.png?text=Mission+${missionId}`}
+            alt="mission-image"
+            className={clsx(
+              "absolute inset-0 w-full h-full object-cover z-[-1]",
+              isLocked && "blur-sm"
+            )}
+          />
+        )}
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex flex-col gap-2">
+            <div className="h-[18px] flex items-center">
+              <span className="px-3 py-1 bg-neutral-300 rounded-3xl text-[10px] font-semibold text-neutral-600">
+                {sectorName}
+              </span>
+            </div>
+            <div className="t-m-56-b text-neutral-100">{difficulty}</div>
+            <div className="text-sm font-medium text-neutral-100">
+              이 루트 궁금하지 않으신가요?
+            </div>
+          </div>
+          <div className="flex justify-end">
+            {isLocked && <Button leftIcon={<LockIcon variant="white" />} />}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
