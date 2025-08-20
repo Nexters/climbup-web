20ff6eaea43e64385d9ad09833f1ffe358d9a2dd
minsgy <alstjrchl97@gmail.com>
2025-08-20 21:54:45 +0900

    feat: 온보딩 이미지 추가 및 Swiper 슬라이더 구현, DotLottie 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,14 @@
-import { type DotLottie, DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { Flex } from "@radix-ui/themes";
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
-import assetLottie from "@/assets/lottie/home-logo.lottie";
+import { motion } from "motion/react";
+import { renderToString } from "react-dom/server";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import type { PaginationOptions } from "swiper/types";
+import assetOnboarding1 from "@/assets/images/ic_onboarding_1.png";
+import assetOnboarding2 from "@/assets/images/ic_onboarding_2.png";
+import assetOnboarding3 from "@/assets/images/ic_onboarding_3.png";
+import { cn } from "@/utils/cn";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -10,39 +16,83 @@ export const Route = createFileRoute("/")({
 
 const KAKAO_LOGIN_URL = `https://dev-api.holdy.kr/login/kakao?redirect_uri=${window.location.origin}/oauth2/redirect`;
 
-const intermission = 1500;
 function Home() {
-  const [dotLottie, setDotLottie] = useState<DotLottie | null>(null);
-
-  useEffect(() => {
-    const onComplete = () => {
-      setTimeout(() => {
-        dotLottie?.play();
-      }, intermission);
-    };
-
-    dotLottie?.addEventListener("complete", onComplete);
-
-    return () => {
-      dotLottie?.removeEventListener("complete", onComplete);
-    };
-  }, [dotLottie]);
-
-  return (
-    <Flex direction="column" gap="5" className="h-full px-10 py-4">
-      <Flex direction="column" align="center" gap="4px" className="mt-[160px]">
-        <DotLottieReact
-          dotLottieRefCallback={setDotLottie}
-          src={assetLottie}
-          autoplay
-          loop={false}
-          className="w-[198px] h-[198px]"
+  const pagination: PaginationOptions = {
+    clickable: true,
+    renderBullet(_, className) {
+      return renderToString(
+        <motion.div
+          className={cn(
+            className,
+            "w-2 h-2 rounded-full bg-neutral-700 transition-[width] duration-200 [&.swiper-pagination-bullet-active]:w-4"
+          )}
         />
-        <h2 className="t-p-22-sb pt-6">홀디에 오신 걸 환영해요 :)</h2>
-        <p className="t-p-14-m">내 페이스에 맞춰 하나씩 올라가 볼까요?</p>
+      );
+    },
+  };
+  return (
+    <Flex direction="column" gap="5" className="h-dvh px-10 py-4">
+      <header className="h-[52px] w-full flex-center t-m-24-b select-none">
+        Holdy
+      </header>
+      <Flex direction="column" align="center" gap="4px" className="h-full pt-2">
+        <Swiper
+          modules={[Pagination]}
+          loop
+          slidesPerView={1}
+          pagination={pagination}
+          className="w-full overflow-hidden"
+          aria-label="메인 프로모션 슬라이더"
+        >
+          <SwiperSlide>
+            <div className="flex flex-col items-center justify-center gap-5 h-full pb-10">
+              <div className="w-full flex-1 flex justify-center items-end">
+                <img
+                  src={assetOnboarding1}
+                  alt="미션 시작 가이드"
+                  className="w-[180px] h-[193px] object-cover"
+                  draggable={false}
+                />
+              </div>
+              <p className="t-p-22-sb text-neutral-900 whitespace-pre-line text-center">
+                {`실력에 꼭 맞는\n루트 미션을 준비했어요`}
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center justify-center gap-5 h-full pb-10">
+              <div className="w-full flex-1 flex justify-center items-end">
+                <img
+                  src={assetOnboarding2}
+                  alt="미션 시작 가이드"
+                  className="w-[140px] h-[157px] object-cover"
+                  draggable={false}
+                />
+              </div>
+              <p className="t-p-22-sb text-neutral-900 whitespace-pre-line text-center ">
+                {`어려웠던 미션은\n해설 영상으로 배워보세요`}
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center justify-center gap-5 h-full pb-10">
+              <div className="w-full flex-1 flex justify-center items-end">
+                <img
+                  src={assetOnboarding3}
+                  alt="미션 시작 가이드"
+                  className="w-[240px] h-[294px] object-cover"
+                  draggable={false}
+                />
+              </div>
+              <p className="t-p-22-sb text-neutral-900 whitespace-pre-line text-center">
+                {`미션을 성공하고\n성취감을 느껴보세요`}
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </Flex>
       <a
-        className="h-[52px] flex-center bg-[#FEE500] rounded-full gap-1 mt-auto"
+        className="h-[52px] flex-center bg-[#FEE500] rounded-full gap-1 mt-auto shrink-0"
         href={KAKAO_LOGIN_URL}
       >
         <svg
--- src/types/swiper.d.ts
+++ src/types/swiper.d.ts
@@ -1,2 +1,4 @@
 declare module "swiper/css";
 declare module "swiper/css/virtual";
+declare module "swiper/css/pagination";
+declare module "swiper/css/*";
