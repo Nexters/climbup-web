1edbb52f2c9656e1f46ff826dc4a9f974a26a45f
minsgy <alstjrchl97@gmail.com>
2025-08-21 00:32:34 +0900

    fix: MissionLockCard 컴포넌트 이미지 및 텍스트 수정

--- src/routes/mission/-components/MissionLockCard.tsx
+++ src/routes/mission/-components/MissionLockCard.tsx
@@ -1,18 +1,27 @@
+import assetMissionIcon from "@/assets/images/ic_lock.png";
+import assetMissionLock from "@/assets/images/ic_lock_image.png";
+
 export default function MissionLockCard() {
   return (
     <div className="card-container">
-      <div className="relative w-full h-full overflow-hidden rounded-[32px]">
-        <img
-          src="/mission-start-mock.png"
-          alt="클라이밍 이미지"
-          className="absolute inset-0 scale-105 w-full h-full object-cover blur-sm rounded-[32px]"
-        />
-        <div className="h-full flex flex-col items-center justify-center gap-2 relative z-1">
-          <img src="/mission-lock.png" alt="자물쇠 이미지" className="w-16" />
-          <p className="text-center t-p-14-sb text-neutral-100 [text-shadow:_0_4px_8px_rgba(0,0,0,0.5)]">
-            재생 버튼을 눌러
+      <div className="relative w-full h-full rounded-[32px]">
+        <div className="h-full flex flex-col items-center justify-center gap-2 relative z-1 bg-neutral-400 rounded-[32px]">
+          <img
+            src={assetMissionIcon}
+            alt="자물쇠 이미지"
+            className="absolute w-[35px] h-[43px] left-[24px] top-[24px] object-cover"
+          />
+          <div className="max-w-[200px] max-h-[200px] w-full h-full aspect-square">
+            <img
+              src={assetMissionLock}
+              alt="미션 아이콘"
+              className="size-full object-cover"
+            />
+          </div>
+          <p className="text-center t-p-16-m text-neutral-800">
+            아래 재생 버튼을 눌러
             <br />
-            오늘의 추천 루트를 확인해보세요
+            루트 미션을 확인해보세요
           </p>
         </div>
       </div>
